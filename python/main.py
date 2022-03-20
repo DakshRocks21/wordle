e9c54f8bea2f733ce94dc5d8642d9cd933304e8c
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2022-03-20 14:36:13 +0800

    Added Colors

--- python/main.py
+++ python/main.py
@@ -1,8 +1,8 @@
 import json
 import random
 from sys import platform
-
-
+import os
+# The Game
 class Wordle():
     def __init__(self):
         if platform == "linux" or platform == "linux2" or platform == "darwin":
@@ -11,12 +11,33 @@ class Wordle():
             self.word_list = json.load(open("python\words.json"))
         else:
             print("Unknown platform")
+            exit()
+
         
     def getWord(self):
         word = self.word_list[random.randint(0, len(self.word_list) - 1)]
+        self.word = word
         return word
+    
+    def getInput(self):
+        user = input("Enter a word: ")
+        return user
 
+    def color(self, color:str, letter:str):
+        if color == "green":
+            print(f"\033[92m{letter}\033[00m", end=" ")
+        elif color == "yellow":
+            print(f"\033[93m{letter}\033[00m", end=" ")
 
+    def checkWord(self, word: str):
+        # Check is the input is 100% correct
+        if word == self.word:
+            for i in word:
+                self.color("green", i)
+            print("\nYou won!\n\n")
+        else:
+            return False
 
+os.system("clear")
 wordle = Wordle()
-print(wordle.getWord())
+wordle.checkWord(wordle.getWord())

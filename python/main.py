a497a74ab2f5b5f60da07c151986f5c54af6af1c
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2022-03-20 14:18:53 +0800

    Added support for windows

--- python/main.py
+++ python/main.py
@@ -1,9 +1,17 @@
 import json
 import random
+from sys import platform
+
 
 class Wordle():
     def __init__(self):
-        self.word_list = json.load(open("python/words.json"))
+        if platform == "linux" or platform == "linux2" or platform == "darwin":
+            self.word_list = json.load(open("python/words.json"))
+        elif platform == "win32":
+            self.word_list = json.load(open("python\words.json"))
+        else:
+            print("Unknown platform")
+        
     def getWord(self):
         word = self.word_list[random.randint(0, len(self.word_list) - 1)]
         return word
